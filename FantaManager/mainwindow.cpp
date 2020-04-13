a6c009fc8d6b715db97e8b8cd5097d79220ed72d
filippomarcucci <45876691+filippomarcucci@users.noreply.github.com>
2020-04-13 18:59:29 +0200

    Nuovi push button
    mainwindow.cpp --> nuovo metodo per settare le icone e la grafica dei QPushButton in tutta la qApp

--- FantaManager/mainwindow.cpp
+++ FantaManager/mainwindow.cpp
@@ -2,13 +2,29 @@
 #include "ui_mainwindow.h"
 #include "qmessagebox.h"
 
-#include <QPushButton>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
+
+    qApp->setStyleSheet(QString("QPushButton         {color: #333333; border: 2px solid #808080; border-style: outset; border-radius: 5px;} "
+                                "QPushButton:hover   {color: #ffffff; border: 2px solid #e6e6e6;} "
+                                "QPushButton:pressed {color: #ffffff; border: 2px solid #808080;}"));
+
+    // Set icon for push buttons
+    ui->rmTeamPb->setIcon(QIcon(":/new/img/Resources/BtnImages/RemoveTeam.png"));
+    ui->addTeamPb->setIcon(QIcon(":/new/img/Resources/BtnImages/AddTeam.png"));
+    ui->expTeamPb->setIcon(QIcon(":/new/img/Resources/BtnImages/ExcelExport.png"));
+    ui->modTeamPb->setIcon(QIcon(":/new/img/Resources/BtnImages/ModifyTeam.png"));
+    ui->rmTeamPb->setIconSize(QSize(32, 32));
+    ui->addTeamPb->setIconSize(QSize(32, 32));
+    ui->expTeamPb->setIconSize(QSize(32, 32));
+    ui->modTeamPb->setIconSize(QSize(32, 32));
+
 }
 
 MainWindow::~MainWindow()
